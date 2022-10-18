d2f97953918dfecb4728e4c2357d5bb7daa3fc57
Eduardo Díaz <ediaz@dcc8090.cl>
2022-10-18 18:06:52 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -16,7 +16,7 @@ public class App {
     if (a == b || a == c || b == c) {
       return "es un triangulo isoceles";
     }
-    return "error no es un triangulo";
+    return "error no es un triangulo" INTRODUCIMOS UN ERROR
   }
 
   public static void main(String[] args) {
